981d343379cd9f4ae4f831ac9f74f402c468a375
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-04-08 20:55:07 +0530

    search recipe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Addrecipe from './components/Addrecipe';
+import Searchrecipe from './components/Searchrecipe';
 
 function App() {
   return (
     <div>
-      <Addrecipe/>
+      <Searchrecipe/>
     </div>
   );
 }
